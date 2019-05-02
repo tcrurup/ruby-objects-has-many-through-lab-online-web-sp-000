2c7db2db1d177b84ac3687a9c4d50d47cccad948
Anthony Rurup <tcrurup@gmail.com>
2019-05-02 22:19:01 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,11 +1,17 @@
 class Genre
   
+  @@all 
+  
   def initialize(genre_name)
     @name = genre_name
+    @@all << self
   end
   
   def name
     @name
   end
   
+  def self.all 
+    @@all
+  end
 end
