bc6fc19688ec6fa9702b3f45a84c0c012818ebf7
Anthony Rurup <tcrurup@gmail.com>
2019-05-02 22:58:02 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,6 +2,26 @@ class Patient
   
   @@all = []
   
+  def initialize(patient_name)
+    @name = patient_name
+    @all << self 
+  end
+  
+  def name 
+    @name 
+  end
+  
+  def new_appointment(doctor, date)
+    #Creates a new appointment, it should know that it belongs to the patient  
+  end
+  
+  def appointments
+    #Iterates through all appoingments and returns ones that belong to the patient 
+  end
+  
+  def doctors
+    #Iterats over all the patients appointments and collects the doctors from each appointments
+  end
   
   def self.all
     @@all
