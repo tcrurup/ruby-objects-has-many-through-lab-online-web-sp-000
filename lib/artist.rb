a73599e46ac639e1a011d916130e5a25000201cd
Anthony Rurup <tcrurup@gmail.com>
2019-05-02 22:26:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,7 +12,7 @@ class Artist
   end
   
   def songs
-    Songs.all.select{ |song| song.artist == self }
+    Song.all.select{ |song| song.artist == self }
   end
   
   def new_song(song_name, song_genre)
